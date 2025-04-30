ddb59a7103aa7480507a4480e3b2428a64e4ac3b
barbedwire <e.barbedwire@gmail.com>
2025-04-30 08:14:04 +0200

    fix typo

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,7 +4,7 @@ const (
 	// DateFormat defines date representation in each operation
 	DateFormat = "2006-01-02"
 
-	// WordsInSet - the minumum amount of words required in the dictionary to start a session
+	// WordsInSet - the minimum amount of words required in the dictionary to start a session
 	WordsInSet = 10
 
 	// Dictionary file name
